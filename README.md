21b058f30eeb86adf57e604dee5a93767277ddf4
Jake Soenneker <jake@soenneker.com>
2023-04-08 14:26:24 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Messages.Base.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Messages.Base/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.messages.base/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.messages.base/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Messages.Base.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Messages.Base/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Messages.Base
+### A fundamental Azure Service Bus message class
+
+## Installation
+
+```
+Install-Package Soenneker.Messages.Base
+```
---
+++ src/Message.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Ardalis.SmartEnum.SystemTextJson;
+using Newtonsoft.Json;
+using Soenneker.Enums.ServiceBusQueue;
+using Soenneker.Utils.Environment;
+
+namespace Soenneker.Messages.Base;
+
+/// <summary>
+/// A fundamental Azure Service Bus message class <para/>
+/// Messages are specific to a particular bus
+/// </summary>
+public abstract class Message
+{
+    [JsonPropertyName("queue")]
+    [JsonProperty("queue")]
+    [System.Text.Json.Serialization.JsonConverter(typeof(SmartEnumNameConverter<ServiceBusQueue, int>))]
+    [Newtonsoft.Json.JsonConverter(typeof(Ardalis.SmartEnum.JsonNet.SmartEnumNameConverter<ServiceBusQueue, int>))]
+    public ServiceBusQueue Queue { get; set; }
+
+    /// <summary>
+    /// Defaults to machine name
+    /// </summary>
+    [JsonPropertyName("sender")]
+    [JsonProperty("sender")]
+    public string Sender { get; set; }
+
+    /// <summary>
+    /// Defaults to false, but should be true with things that can't handle system.text.json yet (i.e. AdaptiveCards)
+    /// </summary>
+    [JsonPropertyName("newtonsoftSerialize")]
+    [JsonProperty("newtonsoftSerialize")]
+    public bool NewtonsoftSerialize { get; set; }
+
+    [JsonPropertyName("createdAt")]
+    [JsonProperty("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    protected Message(ServiceBusQueue queue)
+    {
+        Queue = queue;
+        NewtonsoftSerialize = false;
+        Sender = EnvironmentUtil.GetMachineName();
+        CreatedAt = DateTime.UtcNow;
+    }
+}
